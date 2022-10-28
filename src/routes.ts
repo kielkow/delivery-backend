d40b7d0e5303f4aced9beb9ffcb2d4467fd96ee6
kielkow <matheuskiel@fiorifer.com.br>
2022-10-28 11:55:20 -0300

    feat: authenticate client route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
+
+import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/authenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/createClientController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
 
 routes.post('/clients', createClientController.handle);
+routes.post('/auth', authenticateClientController.handle);
 
 export { routes };
