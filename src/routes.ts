c6a437fc62b951dd6399a2f01935971a483d0c86
kielkow <matheuskiel@fiorifer.com.br>
2022-11-01 10:37:29 -0300

    feat: createDeliveryman route

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/authenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/createClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/createDeliverymanController";
 
 const routes = Router();
 
-const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createClientController = new CreateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 routes.get('/', (request, response) => {
     return response.json({
@@ -14,7 +16,10 @@ routes.get('/', (request, response) => {
     });
 });
 
-routes.post('/clients', createClientController.handle);
 routes.post('/auth', authenticateClientController.handle);
+
+routes.post('/clients', createClientController.handle);
+
+routes.post('/deliveryman', createDeliverymanController.handle);
 
 export { routes };
