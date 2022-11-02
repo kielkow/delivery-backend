dd01c880032828fe66ed9bef49c49bf169d713b0
kielkow <matheuskiel@fiorifer.com.br>
2022-11-02 19:40:59 -0300

    feat: authenticateDeliveryman route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/authenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/useCases/authenticateDeliveryman/authenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/createClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/createDeliverymanController";
 
@@ -8,18 +9,23 @@ const routes = Router();
 
 const authenticateClientController = new AuthenticateClientController();
 const createClientController = new CreateClientController();
+
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 const createDeliverymanController = new CreateDeliverymanController();
 
+// Test server
 routes.get('/', (request, response) => {
     return response.json({
         message: 'delivery-backend server is running'
     });
 });
 
-routes.post('/auth', authenticateClientController.handle);
-
+// Clients
+routes.post('/clients/auth', authenticateClientController.handle);
 routes.post('/clients', createClientController.handle);
 
+// Deliveryman
+routes.post('/deliveryman/auth', authenticateDeliverymanController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
 
 export { routes };
