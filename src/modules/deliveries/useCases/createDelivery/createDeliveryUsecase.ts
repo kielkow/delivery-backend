14f24ba50075ab1a27ebbd3bdd8ef8838f6d30f4
kielkow <matheuskiel@fiorifer.com.br>
2022-11-03 11:00:12 -0300

    feat: createDeliveryUsecase

---
+++ src/modules/deliveries/useCases/createDelivery/createDeliveryUsecase.ts
@@ -0,0 +1,20 @@
+import { Deliveries } from "@prisma/client";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateDelivery {
+    item_name: string;
+    id_client: string;
+}
+
+export class CreateDeliveryUseCase {
+    async execute({ item_name, id_client }: ICreateDelivery): Promise<Deliveries> {
+        const delivery = await prisma.deliveries.create({
+            data: {
+                item_name,
+                id_client,
+            },
+        });
+
+        return delivery;
+    }
+}
