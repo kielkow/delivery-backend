7f5f7343d4b4738f09eb0276cd0d0573efa26791
kielkow <matheuskiel@fiorifer.com.br>
2022-11-03 11:00:32 -0300

    feat: createDelivery route

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/authenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/useCases/authenticateDeliveryman/authenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/createClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/createDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/createDeliverymanController";
 
 const routes = Router();
@@ -12,6 +13,8 @@ const createClientController = new CreateClientController();
 
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 const createDeliverymanController = new CreateDeliverymanController();
+
+const createDeliveryController = new CreateDeliveryController();
 
 // Test server
 routes.get('/', (request, response) => {
@@ -27,5 +30,8 @@ routes.post('/clients', createClientController.handle);
 // Deliveryman
 routes.post('/deliveryman/auth', authenticateDeliverymanController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
+
+// Deliveries
+routes.post('/deliveries', createDeliveryController.handle);
 
 export { routes };
