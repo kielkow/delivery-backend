6f24bc8bf9337a9bf591ff66496df1597a7143a5
kielkow <matheuskiel@fiorifer.com.br>
2022-11-09 11:25:04 -0300

    feat: ensureAuthenticateDeliveryman

---
+++ src/middlewares/ensureAuthenticateDeliveryman.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import AppError from "../AppError";
+
+interface IPayload {
+    sub: string;
+}
+
+export async function ensureAuthenticateDeliveryman(request: Request, response: Response, next: NextFunction) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError('Token missing', 401);
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+        const { sub } = verify(token, 'b5486dcaf4cfc9c003f86209802ba582') as IPayload;
+
+        request.id_deliveryman = sub;
+
+        return next();
+    } catch (error) {
+        throw new AppError('Invalid token', 401);
+    }
+}
